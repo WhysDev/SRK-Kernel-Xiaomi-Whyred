c5d28a39565eff2e7c212b258f56f01bea3cba6a
psndna88 <psndna88@gmail.com>
2021-04-28 20:51:11 +0530

    fixings

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Mon May 17 02:11:58 GMT 2021"
+#define LINUX_COMPILE_BY "WhysDev"
+#define LINUX_COMPILE_HOST "Tom"
+#define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04)"
